aeebee7e17942a6353c64e7fce2b7ded26e824af
Jan Korf <jankorf91@gmail.com>
2024-11-19 13:33:32 +0100

    Client Configuration (#2)
    Updated CryptoExchange.Net to version 8.3.0
    Added support for loading client settings from IConfiguration
    Added DI registration method for configuring Rest and Socket options at the same time
    Added DisplayName and ImageUrl properties to CryptoComExchange class
    Updated client constructors to accept IOptions from DI
    Removed redundant CryptoComSocketClient constructor

--- CryptoCom.Net/Objects/Options/CryptoComSocketOptions.cs
+++ CryptoCom.Net/Objects/Options/CryptoComSocketOptions.cs
@@ -11,27 +11,31 @@ namespace CryptoCom.Net.Objects.Options
         /// <summary>
         /// Default options for new clients
         /// </summary>
-        public static CryptoComSocketOptions Default { get; set; } = new CryptoComSocketOptions()
+        internal static CryptoComSocketOptions Default { get; set; } = new CryptoComSocketOptions()
         {
             Environment = CryptoComEnvironment.Live,
             SocketSubscriptionsCombineTarget = 10,
             DelayAfterConnect = TimeSpan.FromSeconds(1)
         };
 
-        
-         /// <summary>
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public CryptoComSocketOptions()
+        {
+            Default?.Set(this);
+        }
+
+        /// <summary>
         /// Exchange API options
         /// </summary>
         public SocketApiOptions ExchangeOptions { get; private set; } = new SocketApiOptions();
 
-
-        internal CryptoComSocketOptions Copy()
+        internal CryptoComSocketOptions Set(CryptoComSocketOptions targetOptions)
         {
-            var options = Copy<CryptoComSocketOptions>();
-            
-            options.ExchangeOptions = ExchangeOptions.Copy<SocketApiOptions>();
-
-            return options;
+            targetOptions = base.Set<CryptoComSocketOptions>(targetOptions);
+            targetOptions.ExchangeOptions = ExchangeOptions.Set(targetOptions.ExchangeOptions);
+            return targetOptions;
         }
     }
 }
