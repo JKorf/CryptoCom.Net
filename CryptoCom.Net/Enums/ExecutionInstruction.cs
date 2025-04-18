519dc796d511f7c85c4c22218585509e3cd4c218
Jkorf <jankorf91@gmail.com>
2025-04-18 16:37:48 +0200

    Added missing EnumConverter attribute

--- CryptoCom.Net/Enums/ExecutionInstruction.cs
+++ CryptoCom.Net/Enums/ExecutionInstruction.cs
@@ -1,8 +1,10 @@
 ﻿using CryptoExchange.Net.Attributes;
+using CryptoExchange.Net.Converters.SystemTextJson;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CryptoCom.Net.Enums
@@ -10,6 +12,7 @@ namespace CryptoCom.Net.Enums
     /// <summary>
     /// Execution mode
     /// </summary>
+    [JsonConverter(typeof(EnumConverter<ExecutionInstruction>))]
     public enum ExecutionInstruction
     {
         /// <summary>
