e693ccce918c2f3452f5ff0a6736c60756b0b99a
Jan Korf <jankorf91@gmail.com>
2026-02-24 14:00:51 +0100

    CryptoExchange.Net update (#16)
    Updated CryptoExchange.Net to version 10.7.0
    Added additional Http settings to client options
    Updated Shared REST interfaces pagination logic
    Updated HttpClient registration, fixing issue of DNS changes not getting processed
    Fixed UserClientProvider using unconfigured HttpClient

--- CryptoCom.Net/Clients/CryptoComUserClientProvider.cs
+++ CryptoCom.Net/Clients/CryptoComUserClientProvider.cs
@@ -42,6 +42,7 @@ namespace CryptoCom.Net.Clients
             IOptions<CryptoComSocketOptions> socketOptions)
         {
             _httpClient = httpClient ?? new HttpClient();
+            _httpClient.Timeout = restOptions.Value.RequestTimeout;
             _loggerFactory = loggerFactory;
             _restOptions = restOptions;
             _socketOptions = socketOptions;
