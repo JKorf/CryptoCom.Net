88a2b31053516c44a6584d1e30eb0a1d665e4e1b
Jkorf <jankorf91@gmail.com>
2026-09-22 15:38:48 +0200

    wip

--- CryptoCom.Net/Clients/ExchangeApi/Rest/SharedApi/CryptoComRestClientExchangeApiShared.FuturesSymbols.cs
+++ CryptoCom.Net/Clients/ExchangeApi/Rest/SharedApi/CryptoComRestClientExchangeApiShared.FuturesSymbols.cs
@@ -17,7 +17,7 @@ namespace CryptoCom.Net.Clients.ExchangeApi
     {
         #region Get Futures Symbols
 
-        async Task<ICallResult<SharedFuturesSymbol[]>> IGetFuturesSymbols.GetFuturesSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
+        async Task<IExchangeCallResult<SharedFuturesSymbol[]>> IGetFuturesSymbols.GetFuturesSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
             => await GetFuturesSymbolsAsync(request, ct).ConfigureAwait(false);
 
         public SharedSymbolCatalog? FuturesSymbolCatalog => ExchangeSymbolCache.GetSymbolCatalog(_exchangeName, _topicFuturesId, _api.EnvironmentName, null);
